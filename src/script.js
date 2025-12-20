525d1628077a1f47dafeb7d22337f12177cac877
ericocneto <cordeironetoerico@gmail.com>
2025-12-20 20:34:00 -0300

    visual interface

---
+++ src/script.js
@@ -0,0 +1,13 @@
+'use strict'
+
+/*======== DOM LOGS ========*/
+const dom = {
+    spaceStartKey: document.addEventListener("keydown", (e)=> {
+        if(e.code==="Space") {
+            alert(`START BUTTON PRESSIONED!`)
+        }
+    })
+    
+}
+
+/*======== HANDLERS ======== */
