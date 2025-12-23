5fd18018a565de57d7e772a3a6265e4479c9c6ad
ericocneto <cordeironetoerico@gmail.com>
2025-12-23 18:26:40 -0300

    switch player

--- src/script.js
+++ src/script.js
@@ -1,13 +1,28 @@
 'use strict'
 
-/*======== DOM LOGS ========*/
-const dom = {
-    spaceStartKey: document.addEventListener("keydown", (e)=> {
-        if(e.code==="Space") {
-            alert(`START BUTTON PRESSIONED!`)
-        }
-    })
-    
-}
 
-/*======== HANDLERS ======== */
+let activePlayer = 0 // White starts (white === 0)
+/* ======== DOM CACHE ======== */
+const whiteButton = document.getElementById('whiteDisplay')
+const blackButton = document.getElementById('blackDisplay')
+
+
+/* ======== EVENTS ======== */
+let whiteTimeCurrent = 300
+let blackTimeCurrent = 300 
+
+ document.addEventListener('keydown', function(e) {
+  whiteButton.classList.remove('playing--now')
+  blackButton.classList.remove('playing--now')
+
+  if (activePlayer === 0) {
+    whiteButton.classList.add('playing--now')
+    activePlayer = 1
+  } else {
+    blackButton.classList.add('playing--now')
+    activePlayer = 0
+  }
+})
+
+
+/* ======== TIME MANAGEMENT ======== */
